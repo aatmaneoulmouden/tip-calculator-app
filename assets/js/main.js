05cb33df8754849676ec6dfe2a1edde713283e6b
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-04-10 18:02:24 +0000

    Functionality integration P1: Get tip value and test

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,15 +28,59 @@ customTipInput.addEventListener('focus', () => {
  * Calculate the correct tip and total cost of the bill per person
  */
 
-const billInput = document.querySelector('#bill');
-const tipInputs = document.querySelectorAll('input[name=tip]');
-const numOfPeopleInput = document.querySelector('#num-people');
+// const billInput = document.querySelector('#bill');
+// const tipInputs = document.querySelectorAll('input[name=tip]');
+// const numOfPeopleInput = document.querySelector('#num-people');
+
+
+
+// const getTipValue = () => {
+//     let tipValue;
+
+//     if (document.querySelector('input[name=tip]#custom-tip-input').value.length == 0) {
+//         tipValue = document.querySelector('input[name=tip]:checked').value;
+//     } else {
+//         tipValue = document.querySelector('input[name=tip]#custom-tip-input').value;
+//     }
+
+//     console.log(tipValue);
+//     // return tipValue;
+// };
+
+// tipInputs.forEach(tipInput => {
+//     tipInput.addEventListener('change', getTipValue);
+// });
+
+// billInput.addEventListener('input', () => {
+//     // if(billInput.value.length > 0 && tip) {
+
+//     // }
+//     console.log(billInput.value);
+// });
+
+const preDefTipValues = document.querySelectorAll('.tip-option-value');
+// const customTipValue = document.querySelector('#custom-tip-input');
+let tipValue = null;
+
+const updateTipValue = (newValue) => tipValue = newValue;
 
 const calculateTip = (bill, tip, peopleCount) => {
-    let tipAmount = (bill * (tip / 100)) / peopleCount;
+    let tipAmount = (parseInt(bill) * (parseInt(tip) / 100)) / parseInt(peopleCount);
     let total = (bill / peopleCount) + tipAmount;
     return {
         'tipAmount': tipAmount,
         'total': total
     }
 };
+
+preDefTipValues.forEach(tip => {
+    tip.addEventListener('change', () => {
+        let htip = updateTipValue(tip.value);
+        let result = calculateTip(100, htip, 5);
+        console.log(result);
+    });
+});
+
+customTipInput.addEventListener('input', () => {
+    updateTipValue(customTipInput.value);
+});
